765d891a87c7668bc72e61b8215e1bc6fff54e1a
TorchaSpace <ali.emre.celix@gmail.com>
2023-09-13 13:19:37 +0300

    PlayerController

---
+++ alictushub_devcase/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private FixedJoystick _joystick;
+    [SerializeField] private Animator _animator;
+
+    [SerializeField] private float _speed;
+
+    private void FixedUpdate()
+    {
+        float horizontalInput = _joystick.Horizontal;
+        float verticalInput = _joystick.Vertical;
+
+        Vector3 moveDirection = new Vector3(horizontalInput * _speed, _rigidbody.velocity.y, verticalInput * _speed);
+
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+            SetIsRunning(true);
+        }
+        else
+        {
+            SetIsRunning(false);
+        }
+
+        _rigidbody.velocity = moveDirection;
+    }
+
+    private void SetIsRunning(bool isRunning)
+    {
+        _animator.SetBool("isRunning", isRunning);
+    }
+}
